4f7f033553555391b3d618cfce1d0369e28cf053
Agalya7 <31789778+Agalya7@users.noreply.github.com>
2019-03-15 11:17:27 +0530

    Update SLIC.py
    Finding x, y from LAB image

--- SLIC.py
+++ SLIC.py
@@ -11,7 +11,42 @@ import matplotlib.pyplot as plt
 import cv2
 import math
 import numpy as np
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 10:23:05 2019
 
+@author: 15pt03
+"""
+
+#SLIC Superpixels
+
+import matplotlib.pyplot as plt
+import cv2
+import math
+import numpy as np
+
+def SLIC(img, K):
+	l, a, b = cv2.split(img)
+	N = img.shape[0] * img.shape[1]
+	S = math.sqrt(N/K)
+	print(S**2)
+	k_values = np.arange(1, K, S)
+	print(img[1][0])
+	print([i for i, lst in enumerate(img) if [238, 125, 126] in lst][0])
+	#print("Index:", img.index([238, 125, 126])
+	#for k in k_values:
+		#C.append([l[k], a[k], b[k], )
+	#for k in k_values:
+		#d_tab = math.sqrt((l[k] - l)**2 + ()**2 + ()**2)
+	#retrun 0
+    
+img = cv2.imread("img01.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+print(img[0][0])
+#plt.imshow(img)
+print (img.shape)
+
+SLIC(img, max(img.shape[0], img.shape[1]))
 def SLIC(img, K):
     N = img.shape[0] * img.shape[1]
     S = math.sqrt(N/K)
