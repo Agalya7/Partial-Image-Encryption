fa64def614b55a0f2aff34d84ccefb28d7e5279d
Agalya7 <31789778+Agalya7@users.noreply.github.com>
2019-03-20 04:43:23 -0700

    Update SLIC.py

--- SLIC.py
+++ SLIC.py
@@ -11,6 +11,7 @@ Created on Fri Mar 15 10:23:05 2019
 import cv2
 import math
 import numpy as np
+from numpy.linalg import norm
 
 def superPixel(img, K):
     m = 10
@@ -21,32 +22,42 @@ def superPixel(img, K):
     C = []
     prev = 0
     threshold = 0.5
+    print (len(img), len(img[0]))
+    index, kx, ky = 0, 0, 0
     for k in range(K):
         t = k * S
-        ky = t % len(img[0])
-        kx = int((t - ky) / len(img[0]))
+        #ky = t % len(img[0])
+        ky += S
+        #kx = int((t - ky) / len(img[0]))
+        kx = index*S
+        if (abs(ky - len(img[0])) < S):
+            ky = 0
+            index += 1
+        #print (kx, ky)
         C.append([l[kx][ky], a[kx][ky], b[kx][ky], kx, ky])
-    print(C)
+    #print(C)
     while(1):
         D_s = [[float('inf') for i in range(len(img[0]))] for j in range(len(img))]
-        #[[float('inf')] * len(img[0])] * len(img)
         C_c = [[0 for i in range(len(img[0]))] for j in range(len(img))]
-        #[[0] * len(img[0])] * len(img)
         C_sum = [[0 for i in range(len(C[0]))] for j in range(len(C))]
-        #[[0] * len(C[0])] * len(C)
         k_sum = [0 for i in range(K)]
-        #[0] * K
         new_img = [[0 for i in range(len(img[0]))] for j in range(len(img))]
-        #[[0] * len(img[0])] * len(img)
+        index, kx, ky = 0, 0, 0
         for k in range(K):
             t = k * S
-            ky = t % len(img[0])
-            kx = int((t - ky) / len(img[0]))
+            #ky = t % len(img[0])
+            ky += S
+            #kx = int((t - ky) / len(img[0]))
+            kx += index*S
+            if (abs(ky - len(img[0])) < S):
+                ky = 0
+                index += 1
             c0 = C[k][0]
             c1 = C[k][1]
             c2 = C[k][2]
             c3 = C[k][3]
             c4 = C[k][4]
+            
             #specify start as 0 or kx-2*S
             istart = kx-2*S if (kx-2*S) >= 0 else 0
             iend = kx+2*S if (kx+2*S) < len(img) else len(img) - 1
@@ -62,14 +73,17 @@ def superPixel(img, K):
                     if (d_s < D_s[i][j]):
                         D_s[i][j] = d_s
                         C_c[i][j] = k
-                        new_img[i][j] = C[k][:3]
+                        img[i][j] = C[k][:3]
         for i in range(len(img)):
             for j in range(len(img[0])):
-                D_s[i][j] = D_s[i][j-1] if (np.isinf(D_s[i][j])) else D_s[i][j+1]
+                k = (j - 1) if j>(len(img)//2) else j+1
+                while(np.isinf(D_s[i][j])):
+                    D_s[i][j] = D_s[i][k]
+                    k -= 1
         #print (D_s[:100][:100])
         #too many inf values in D_s
         current = np.sum(D_s)
-        print(current, np.isinf(D_s))
+        print(current, np.isinf(D_s))       #still getting inf values
         #print (C_c[:100][:100])
         #calculating new clusters
         for i in range(len(img)):
@@ -93,8 +107,8 @@ def superPixel(img, K):
                 break
         prev = current
         #print((current - prev) / prev)
-    #print (new_img[:10][:10])
-    #cv2.imshow('Complex', new_img)
+    #print (img[:3][:3])
+    #cv2.imshow('Complex', img)
 
 def SLIC(img):
     l, a, b = cv2.split(img)
