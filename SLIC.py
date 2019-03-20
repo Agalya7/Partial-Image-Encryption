866735a21cdb624f410aa9e6dbe73777c4c91c93
Agalya7 <31789778+Agalya7@users.noreply.github.com>
2019-03-20 07:43:27 -0700

    Update SLIC.py

--- SLIC.py
+++ SLIC.py
@@ -7,13 +7,14 @@ Created on Fri Mar 15 10:23:05 2019
 
 #SLIC Superpixels
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import cv2
 import math
 import numpy as np
 from numpy.linalg import norm
 
-def superPixel(img, K):
+def superPixel(image, K):
+    img = image
     m = 10
     N = len(img) * len(img[0])
     S = int(round(math.sqrt(N/K)))
@@ -21,33 +22,27 @@ def superPixel(img, K):
     l, a, b = cv2.split(img)
     C = []
     prev = 0
-    threshold = 0.5
+    threshold = 0.05
     print (len(img), len(img[0]))
     index, kx, ky = 0, 0, 0
     for k in range(K):
         t = k * S
-        #ky = t % len(img[0])
         ky += S
-        #kx = int((t - ky) / len(img[0]))
         kx = index*S
         if (abs(ky - len(img[0])) < S):
             ky = 0
             index += 1
         #print (kx, ky)
         C.append([l[kx][ky], a[kx][ky], b[kx][ky], kx, ky])
-    #print(C)
     while(1):
         D_s = [[float('inf') for i in range(len(img[0]))] for j in range(len(img))]
         C_c = [[0 for i in range(len(img[0]))] for j in range(len(img))]
         C_sum = [[0 for i in range(len(C[0]))] for j in range(len(C))]
         k_sum = [0 for i in range(K)]
-        new_img = [[0 for i in range(len(img[0]))] for j in range(len(img))]
         index, kx, ky = 0, 0, 0
         for k in range(K):
             t = k * S
-            #ky = t % len(img[0])
             ky += S
-            #kx = int((t - ky) / len(img[0]))
             kx += index*S
             if (abs(ky - len(img[0])) < S):
                 ky = 0
@@ -57,13 +52,11 @@ def superPixel(img, K):
             c2 = C[k][2]
             c3 = C[k][3]
             c4 = C[k][4]
-            
             #specify start as 0 or kx-2*S
             istart = kx-2*S if (kx-2*S) >= 0 else 0
             iend = kx+2*S if (kx+2*S) < len(img) else len(img) - 1
             jstart = ky-2*S if (ky-2*S) >= 0 else 0
             jend = ky+2*S if (ky+2*S) < len(img[0]) else len(img[0]) - 1
-            #print (istart, iend, jstart, jend)
             #instead of calculating for every pixel, we choose this particular range
             for i in range(istart, iend):
                 for j in range(jstart, jend):
@@ -76,15 +69,10 @@ def superPixel(img, K):
                         img[i][j] = C[k][:3]
         for i in range(len(img)):
             for j in range(len(img[0])):
-                k = (j - 1) if j>(len(img)//2) else j+1
-                while(np.isinf(D_s[i][j])):
-                    D_s[i][j] = D_s[i][k]
-                    k -= 1
-        #print (D_s[:100][:100])
-        #too many inf values in D_s
+                if (np.isinf(D_s[i][j])):
+                    D_s[i][j] = 0
         current = np.sum(D_s)
-        print(current, np.isinf(D_s))       #still getting inf values
-        #print (C_c[:100][:100])
+        print(current)#, np.isinf(D_s)
         #calculating new clusters
         for i in range(len(img)):
             for j in range(len(img[0])):
@@ -95,27 +83,26 @@ def superPixel(img, K):
                 C_sum[k][3] += i
                 C_sum[k][4] += j
                 k_sum[k] += 1
-        #print ("C_SUM:", C_sum)
         for k in range(K):
             if (k_sum[k] > 0):
                 C[k][:] = [x / k_sum[k] for x in C_sum[k]]
             else:
                 C[k][:] = [0*x for x in C_sum[k]]
-        #print ("->", C, np.sum(k_sum))
         if not (prev == 0):
-            if (((current - prev) / prev) > threshold):
+            #print("H", (current - prev) / prev)
+            if (abs((current - prev) / prev) < threshold):
                 break
         prev = current
-        #print((current - prev) / prev)
-    #print (img[:3][:3])
-    #cv2.imshow('Complex', img)
-
-def SLIC(img):
-    l, a, b = cv2.split(img)
-    superPixel(img, 500)
+        img = cv2.cvtColor(img, cv2.COLOR_Lab2RGB)
+        #print (img[100:][:])
+        plt.imshow(img)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2Lab)
+    return img
     
 img = cv2.imread("img01.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+img = cv2.cvtColor(img, cv2.COLOR_RGB2Lab)
 #plt.imshow(img)
 print (img.shape)
-SLIC(img)
+#superPixel(img, 20)
+print ("Done")
+#plt.imshow(img)
