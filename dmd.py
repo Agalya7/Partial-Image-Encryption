a16c0ec6acf3281366418151071c70a75069ef7b
Agalya7 <31789778+Agalya7@users.noreply.github.com>
2019-03-25 22:45:34 -0700

    Update dmd.py

--- dmd.py
+++ dmd.py
@@ -8,11 +8,13 @@ Created on Mon Mar 25 19:59:01 2019
 import matplotlib.pyplot as plt
 import cv2
 import math
+import os
 import numpy as np
 from numpy.linalg import norm
 import scipy.misc
 
-img = np.float32(cv2.imread("img03.jpg", cv2.IMREAD_COLOR))/255
+img_name = "img03"
+img = np.float32(cv2.imread(img_name + ".jpg", cv2.IMREAD_COLOR))/255
 
 lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
 l, a, b = cv2.split(lab)
@@ -68,11 +70,24 @@ y_svd = np.array(y)
 lu, ls, lvh = np.linalg.svd(l, full_matrices=True)
 yu, ys, yvh = np.linalg.svd(y_svd, full_matrices=True)
 print(l.shape, lu.shape, ls.shape, lvh.shape)
-rank = 20
-svd_decomp = np.matrix(lu[:, :rank]) * np.diag(ls[:rank]) * np.matrix(lvh[:rank, :])
-print(svd_decomp)
-np.save("decomposed.npy", svd_decomp)
-scipy.misc.imsave("decomposed.png", svd_decomp)
+I = [[0 for i in range(20-3+1)] for j in range(l.shape[0] * l.shape[1])]
+col = 0
+for rank in range(3, 4):
+    svd_decomp = np.matrix(lu[:, :rank]) * np.diag(ls[:rank]) * np.matrix(lvh[:rank, :])
+    mofified_svd = np.array(svd_decomp.flatten())[0]
+    print (len(I), len(I[0]), len(mofified_svd))
+    I = np.hstack((I, mofified_svd))
+    #np.append(I, svd_decomp.flatten(), axis=1)
+    #print(svd_decomp)
+    rank_name = str(rank)
+    file_name = img_name + "_" + rank_name
+    np.save(file_name + "_decomp.npy", svd_decomp)
+    scipy.misc.imsave(file_name + "_decomp.png", svd_decomp)
+    os.remove(file_name + "_decomp.npy")
+    
+    col += 1
+print ([row[0] for row in I])
+    #lu, ls, lvh = np.linalg.svd(svd_decomp, full_matrices=True)
 #print(np.allclose(l_svd, np.dot(lu[:, :l.shape[1]] * ls, lvh)))
 #smat = np.zeros((lu.shape, lvh.shape), dtype=complex)
 
